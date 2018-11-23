fb16c37dd49dfb997e9177724a387ebbab8ee7ad
Rick <31820997+Rickvdg@users.noreply.github.com>
2018-11-23 01:25:49 +0100

    Particle window size takes up the whole screen on load

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1506,8 +1506,8 @@ window.particlesJS = function(tag_id, params){
   canvas_el.className = pJS_canvas_class;
 
   /* set size canvas */
-  canvas_el.style.width = "100%";
-  canvas_el.style.height = "100%";
+  canvas_el.style.width = "" + window.innerWidth + "px";
+  canvas_el.style.height = "" + window.innerHeight + "px";
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
